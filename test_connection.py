e4a93e4b0821eeddf5834212fb1d8bbb1491cc3f
Real_acmkan <34609309+real-acmkan@users.noreply.github.com>
2018-09-30 20:35:08 -0600

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -1,7 +1,7 @@
 import httplib
 import socket
 
-
+# define some variables
 test_con_url = "www.google.com" # For connection testing
 test_con_resouce = "/intl/en/policies/privacy/" # may change in future
 test_con = httplib.HTTPConnection(test_con_url) # create a connection
@@ -9,7 +9,7 @@ test_con = httplib.HTTPConnection(test_con_url) # create a connection
 
 try:
     test_con.request("GET", test_con_resouce) # do a GET request
-    response = test_con.getresponse()
+    response = test_con.getresponse() # get the response from the GET request
 except httplib.ResponseNotReady as e:
     print "Improper connection state"
 except socket.gaierror as e:
